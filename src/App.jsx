731b7877bc627df442d8410bb6fcd3a73d966bef
Shripad21 <shripadraccha143@gmail.com>
2025-06-19 23:52:30 +0530

    update bg of header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header.jsx';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-800 via-gray-800 to-black">
+    <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-900 via-gray-800 to-black">
       <Header />
 
       <main className="min-h-screen">
